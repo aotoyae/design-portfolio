d00866a99f7e69ce1661a14e798000137adc6667
kimsohyeon <aotoyae@gmail.com>
2025-02-11 21:49:57 +0900

    fix: 1 패널 기본 설정, 현재 패널 다시 클릭 시 1 패널로 이동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,26 @@ import { useState } from 'react';
 import { motion } from 'framer-motion';
 
 const accordionData = [
-  { title: 'Sohyeon Kim', content: '첫 번째 패널의 내용입니다.' },
-  { title: '패널 2', content: '두 번째 패널의 내용입니다.' },
-  { title: '패널 3', content: '세 번째 패널의 내용입니다.' },
-  { title: '패널 4', content: '네 번째 패널의 내용입니다.' },
-  { title: '패널 5', content: '다섯 번째 패널의 내용입니다.' },
+  { title: 'sohyeon kim', content: '첫 번째 패널의 내용입니다.' },
+  { title: 'web', content: '두 번째 패널의 내용입니다.' },
+  { title: '2D', content: '세 번째 패널의 내용입니다.' },
+  { title: '3D', content: '네 번째 패널의 내용입니다.' },
+  { title: 'editorial', content: '다섯 번째 패널의 내용입니다.' },
 ];
 
 export default function Home() {
-  const [openIndex, setOpenIndex] = useState<number | null>(null);
+  const [openIndex, setOpenIndex] = useState<number | null>(0);
 
   const toggleAccordion = (index: number) => {
-    setOpenIndex(openIndex === index ? null : index);
+    if (openIndex === index) {
+      setOpenIndex(0); // 현재 열린 패널을 클릭하면 패널 1로 이동
+    } else {
+      setOpenIndex(index); // 클릭한 패널로 변경
+    }
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex items-center justify-center min-h-screen">
       <div className="flex w-full overflow-hidden border rounded-lg h-[100vh]">
         {accordionData.map((item, index) => (
           <motion.div
@@ -27,7 +31,7 @@ export default function Home() {
             className="relative flex items-center justify-center overflow-hidden border-r border-gray-300 cursor-pointer"
             onClick={() => toggleAccordion(index)}
             initial={false} // 첫 클릭 시에도 애니메이션 적용
-            animate={{ flex: openIndex === index ? 10 : 1 }}
+            animate={{ flex: openIndex === index ? 12 : 1 }}
             transition={{ duration: 0.5, ease: 'easeInOut' }}
           >
             <p className="absolute text-lg font-semibold vertical-english top-2 right-2">
