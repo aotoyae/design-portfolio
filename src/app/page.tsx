9a30c4c9c401873dae4e6c5bb845d04b0a69fdd1
kimsohyeon <aotoyae@gmail.com>
2025-02-11 22:01:02 +0900

    style: 메뉴별 컬러 설정, 클릭 시 화이트로

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,42 +11,50 @@ const accordionData = [
   { title: 'editorial', content: '다섯 번째 패널의 내용입니다.' },
 ];
 
-export default function Home() {
-  const [openIndex, setOpenIndex] = useState<number | null>(0);
+const menuColors = [
+  'bg-[#A5CBFF]',
+  'bg-[#FF2424]',
+  'bg-[#FF859A]',
+  'bg-[#FCFFD0]',
+  'bg-[#D9D9D9]',
+];
 
-  const toggleAccordion = (index: number) => {
-    if (openIndex === index) {
-      setOpenIndex(0); // 현재 열린 패널을 클릭하면 패널 1로 이동
+export default function Home() {
+  const [openIdx, setOpenIdx] = useState<number | null>(0);
+
+  const toggleAccordion = (idx: number) => {
+    if (openIdx === idx) {
+      setOpenIdx(0); // 현재 열린 패널을 클릭하면 패널 1로 이동
     } else {
-      setOpenIndex(index); // 클릭한 패널로 변경
+      setOpenIdx(idx); // 클릭한 패널로 변경
     }
   };
 
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="flex w-full overflow-hidden border rounded-lg h-[100vh]">
-        {accordionData.map((item, index) => (
+      <div className="flex w-full h-[100vh]">
+        {accordionData.map((item, idx) => (
           <motion.div
-            key={index}
-            className="relative flex items-center justify-center overflow-hidden border-r border-gray-300 cursor-pointer"
-            onClick={() => toggleAccordion(index)}
+            key={idx}
+            className={`relative flex items-center justify-center cursor-pointer ${openIdx === idx ? 'bg-white' : menuColors[idx]}`}
+            onClick={() => toggleAccordion(idx)}
             initial={false} // 첫 클릭 시에도 애니메이션 적용
-            animate={{ flex: openIndex === index ? 12 : 1 }}
+            animate={{ flex: openIdx === idx ? 12 : 1 }}
             transition={{ duration: 0.5, ease: 'easeInOut' }}
           >
-            <p className="absolute text-lg font-semibold vertical-english top-2 right-2">
+            <p className="absolute vertical-english top-2 right-2">
               {item.title}
             </p>
             <motion.div
               className="p-4 text-center text-gray-700"
               initial={{ opacity: 0, scale: 0.95 }}
               animate={{
-                opacity: openIndex === index ? 1 : 0,
-                scale: openIndex === index ? 1 : 0.95,
+                opacity: openIdx === idx ? 1 : 0,
+                scale: openIdx === idx ? 1 : 0.95,
               }}
               transition={{ duration: 0.3, ease: 'easeInOut' }}
             >
-              {openIndex === index && item.content}
+              {openIdx === idx && item.content}
             </motion.div>
           </motion.div>
         ))}
