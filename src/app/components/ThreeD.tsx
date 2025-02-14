1852143798d29303ce6689f3910c875585f2e635
kimsohyeon <aotoyae@gmail.com>
2025-02-14 17:49:31 +0900

    fix: 3d 이미지 추가 및 간격 수정

--- src/app/components/ThreeD.tsx
+++ src/app/components/ThreeD.tsx
@@ -3,24 +3,30 @@ import helmet1 from '@/app/assets/threeD/helmet_1.png';
 import helmet2 from '@/app/assets/threeD/helmet_2.png';
 import snowball1 from '@/app/assets/threeD/snowball_1.jpg';
 import snowball2 from '@/app/assets/threeD/snowball_2.jpg';
-import vans from '@/app/assets/threeD/vans.jpg';
 import watch1 from '@/app/assets/threeD/watch_1.jpg';
 import watch2 from '@/app/assets/threeD/watch_2.jpg';
+import tae1 from '@/app/assets/threeD/tae_1.png';
+import tae2 from '@/app/assets/threeD/tae_2.png';
+import modoriteo1 from '@/app/assets/threeD/modoriteo_1.png';
+import modoriteo2 from '@/app/assets/threeD/modoriteo_2.png';
+import modoriteo3 from '@/app/assets/threeD/modoriteo_3.png';
+import modoriteo4 from '@/app/assets/threeD/modoriteo_4.png';
 
 const ThreeD = () => {
   return (
-    <main className="pt-10 pb-48 w-full flex flex-col gap-48">
+    <main className="pt-10 pb-48 w-full flex flex-col gap-20">
       <Image src={helmet1} alt="투구 모델링" />
       <Image src={helmet2} alt="투구 모델링" />
       <Image src={snowball1} alt="스노우볼 모델링" />
-      <Image
-        src={snowball2}
-        alt="스노우볼 모델링"
-        className="pl-14 pr-40 pb-20"
-      />
-      <Image src={vans} alt="반스 물병 모델링" className="pl-60 pr-20" />
+      <Image src={snowball2} alt="스노우볼 모델링" />
       <Image src={watch2} alt="시계 모델링" />
-      <Image src={watch1} alt="시계 모델링" className="pl-20 pr-32" />
+      <Image src={watch1} alt="시계 모델링" />
+      <Image src={tae1} alt="행거 모델링" />
+      <Image src={tae2} alt="행거 모델링" />
+      <Image src={modoriteo1} alt="문화공간 모델링" />
+      <Image src={modoriteo2} alt="문화공간 모델링" />
+      <Image src={modoriteo3} alt="문화공간 모델링" />
+      <Image src={modoriteo4} alt="문화공간 모델링" />
     </main>
   );
 };
