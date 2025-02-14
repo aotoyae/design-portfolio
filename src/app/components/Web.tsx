db1da6eb9942ee94df2d25b3b70a1c1743d5cfc8
kimsohyeon <aotoyae@gmail.com>
2025-02-15 07:08:53 +0900

    feat: web mori 모바일 이미지 추가

--- src/app/components/Web.tsx
+++ src/app/components/Web.tsx
@@ -13,6 +13,10 @@ import moriWebLayout2 from '@/app/assets/web/mori_web_lighting_2.png';
 import moriWebMain from '@/app/assets/web/mori_web_all.png';
 import moriWebProduct from '@/app/assets/web/mori_web_table.png';
 import moriWebExposition from '@/app/assets/web/mori_web_exposition.png';
+import moriMobileLayout from '@/app/assets/web/mori_mobile_chair.png';
+import moriMobileMain from '@/app/assets/web/mori_mobile_home.png';
+import moriMobileExposition from '@/app/assets/web/mori_mobile_exposition.png';
+import moriMobileMenu from '@/app/assets/web/mori_mobile_menu.png';
 
 const Web = () => {
   return (
@@ -140,8 +144,45 @@ const Web = () => {
             <Image src={moriWebLayout2} alt="모리 데스크탑/테블릿 레이아웃" />
           </div>
           <Image src={moriWebMain} alt="모리 웹 메인 페이지" />
-          <Image src={moriWebProduct} alt="모리 메인 제품 페이지" />
+          <Image src={moriWebProduct} alt="모리 제품 페이지" />
           <Image src={moriWebExposition} alt="모리 메인 전시 안내 페이지" />
+        </section>
+        <section className="pb-10 grid grid-cols-4 gap-2">
+          <article className="pl-3 pr-1">
+            <h6 className="font-bold pb-2">01 MAIN PAGE</h6>
+            <p className="leading-relaxed">
+              그랑핸드의 메인 페이지입니다.
+              <br />
+              베스트/큐레이션 제품, 브랜드 스토리를 확인할 수 있으며, 다섯 곳의
+              매장 사진에 커서를 올리면 위치가 나타납니다.
+            </p>
+          </article>
+          <article className="pl-3 pr-1 col-span-2">
+            <h6 className="font-bold pb-2">02 SUB PAGE</h6>
+            <p className="leading-relaxed">
+              그랑핸드의 제품 리스트 페이지입니다.
+              <br />
+              제품들을 세 열로 정렬하고 제품명, 가격, 용량을 표시하였습니다.
+            </p>
+          </article>
+          <article className="pl-3 pr-1">
+            <h6 className="font-bold pb-2">04 LOGIN PAGE</h6>
+            <p className="leading-relaxed">
+              그랑핸드의 로그인 페이지입니다.
+              <br />
+              밋밋해 보일 수 있는 화면에 긴 배너 이미지를 넣어주었고, 소셜
+              로그인 버튼을 추가하였습니다.
+            </p>
+          </article>
+        </section>
+        <section className="grid grid-cols-4">
+          <Image src={moriMobileLayout} alt="모리 모바일 레이아웃" />
+          <Image src={moriMobileMain} alt="모리 모바일 메인 페이지" />
+          <Image
+            src={moriMobileExposition}
+            alt="모리 모바일 전시 안내 페이지"
+          />
+          <Image src={moriMobileMenu} alt="모리 모바일 메뉴" />
         </section>
       </section>
     </main>
